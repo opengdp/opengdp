51917b886b3581f1c1be5146a50b6cb9e0551913
winkey <rush@winkey.org>
2010-11-27 15:54:33 +0000

    make source image srs testing more robust

--- generic/dwh-image-generic.bash
+++ generic/dwh-image-generic.bash
@@ -73,7 +73,8 @@ function dosubimg {
 
     ##### test the projection ####
        
-    if ! echo $info | grep 'GEOGCS."WGS 84", DATUM."WGS_1984", SPHEROID."WGS 84",6378137,298.257223563, AUTHORITY."EPSG","7030".., AUTHORITY."EPSG","6326".., PRIMEM.."Greenwich",0.*., UNIT."degree",0.0174532925199433., AUTHORITY."EPSG","4326"..' > /dev/null
+    if ! echo $info | grep 'GEOGCS."WGS 84", DATUM."WGS_1984", SPHEROID."WGS 84",6378137,298.257223563,.* PRIMEM.*"Greenwich",0.*UNIT[[]*"degree".*AUTHORITY."EPSG","4326"' > /dev/null
+                      #GEOGCS."WGS 84", DATUM."WGS_1984", SPHEROID."WGS 84",6378137,298.257223563, AUTHORITY."EPSG","7030".., AUTHORITY."EPSG","6326".., PRIMEM.."Greenwich",0.*., UNIT."degree",0.0174532925199433., AUTHORITY."EPSG","4326"..' > /dev/null
     then
 
         ##### does the image not have an alpha band? #####
@@ -397,7 +398,6 @@ function doimg {
     ##### is the img too big? #####
     
     ((cutat=16384))
-    
     
     if [[ $x -gt $cutat ]] || [[ $y -gt $cutat ]]
     then
