ce40ab2e0ae3212a328da1aa3fdd36e900e03921
winkey <rush@winkey.org>
2011-05-14 09:10:08 +0000

    rework otbPanSharp to use the gdal band type detection add generic multiband code add istrue function for testing vars use the new multiband code for landsat 7

--- src/otbPanSharp.cpp
+++ src/otbPanSharp.cpp
@@ -1,23 +1,23 @@
 /*=========================================================================
 
-  Program:   ORFEO Toolbox
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
 
 
-  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
-  See OTBCopyright.txt for details.
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+     See OTBCopyright.txt for details.
 
-  Some parts of this code are derived from ITK. See ITKCopyright.txt
-  for details.
+     Some parts of this code are derived from ITK. See ITKCopyright.txt
+     for details.
 
 
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
 
-=========================================================================*/
+     =========================================================================*/
 
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
@@ -27,192 +27,299 @@
 #define ITK_LEAN_AND_MEAN
 #endif
 
-
 #include "otbImage.h"
 #include "otbVectorImage.h"
 #include "otbImageFileReader.h"
 #include "otbStreamingImageFileWriter.h"
 #include "otbSimpleRcsPanSharpeningFusionImageFilter.h"
 
-
 #include "otbPrintableImageFilter.h"
 #include "itkRescaleIntensityImageFilter.h"
 #include "otbImageToVectorImageCastFilter.h"
 #include "otbVectorRescaleIntensityImageFilter.h"
 
+#include "gdal.h"
 
+void pansharp_byte(char *panfile, char *msfile, char *outfile) {
+    std::cerr << panfile << std::endl;
+    std::cerr << msfile << std::endl;
+    std::cerr << outfile << std::endl;
+    
     typedef otb::Image<double, 2>                   ImageType;
     typedef otb::VectorImage<double, 2>             VectorImageType;
     typedef otb::ImageFileReader<ImageType>         ReaderType;
     typedef otb::ImageFileReader<VectorImageType>   ReaderVectorType;
+    typedef otb::VectorImage<GByte, 2> VectorIntImageType;
 
-    /***** output types *****/
-    
-    typedef otb::VectorImage<unsigned char, 2> VectorByteImageType;
-    typedef otb::VectorImage<unsigned short int, 2> VectorUint16ImageType;
-    typedef otb::VectorImage<signed short int, 2> VectorInt16ImageType;
-    typedef otb::VectorImage<unsigned int, 2> VectorUint32ImageType;
-    typedef otb::VectorImage<signed int, 2> VectorInt32ImageType;
-
-    /***** fusion filter types *****/
-
-    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
-        <ImageType, VectorImageType, VectorByteImageType> FusionByteFilterType;
-    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
-        <ImageType, VectorImageType, VectorUint16ImageType> FusionUint16FilterType;
-    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
-        <ImageType, VectorImageType, VectorInt16ImageType> FusionInt16FilterType;
-    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
-        <ImageType, VectorImageType, VectorUint32ImageType> FusionUint32FilterType;
-    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
-        <ImageType, VectorImageType, VectorInt32ImageType> FusionInt32FilterType;
-
-    /***** writer types *****/
-
-    typedef otb::StreamingImageFileWriter<VectorByteImageType> WriterByteType;
-    typedef otb::StreamingImageFileWriter<VectorUint16ImageType> WriterUint16Type;
-    typedef otb::StreamingImageFileWriter<VectorInt16ImageType> WriterInt16Type;
-    typedef otb::StreamingImageFileWriter<VectorUint32ImageType> WriterUint32Type;
-    typedef otb::StreamingImageFileWriter<VectorInt32ImageType> WriterInt32Type;
-
-
-void workByte(int argc, char* argv[]) {
+    /***** setup reader for pan band *****/
 
     ReaderType::Pointer       readerPAN = ReaderType::New();
-    readerPAN->SetFileName(argv[2]);
-    
-    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
-    readerXS->SetFileName(argv[3]);
+    readerPAN->SetFileName(panfile);
 
-    FusionByteFilterType::Pointer fusion = FusionByteFilterType::New();
+    /***** setup reader for ms band *****/
+
+    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
+    readerXS->SetFileName(msfile);
+
+    /***** setup the filter *****/
+
+    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
+        <ImageType, VectorImageType, VectorIntImageType> FusionFilterType;
+
+    FusionFilterType::Pointer fusion = FusionFilterType::New();
+
+    /***** tie the inputs to the filter *****/
+
     fusion->SetPanInput(readerPAN->GetOutput());
     fusion->SetXsInput(readerXS->GetOutput());
 
-    WriterByteType::Pointer writer = WriterByteType::New();
+    /***** setup output *****/
 
-    writer->SetFileName(argv[3]);
+    typedef otb::StreamingImageFileWriter<VectorIntImageType> WriterType;
+
+    WriterType::Pointer writer = WriterType::New();
+
+    writer->SetFileName(outfile);
     writer->SetInput(fusion->GetOutput());
     writer->Update();
 
 }
 
-void workUint16(int argc, char* argv[]) {
+void pansharp_uint16(char *panfile, char *msfile, char *outfile) {
+    
+    typedef otb::Image<double, 2>                   ImageType;
+    typedef otb::VectorImage<double, 2>             VectorImageType;
+    typedef otb::ImageFileReader<ImageType>         ReaderType;
+    typedef otb::ImageFileReader<VectorImageType>   ReaderVectorType;
+    typedef otb::VectorImage<GUInt16, 2> VectorIntImageType;
+
+    /***** setup reader for pan band *****/
 
     ReaderType::Pointer       readerPAN = ReaderType::New();
-    readerPAN->SetFileName(argv[2]);
-    
-    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
-    readerXS->SetFileName(argv[3]);
+    readerPAN->SetFileName(panfile);
 
-    FusionUint16FilterType::Pointer fusion = FusionUint16FilterType::New();
+    /***** setup reader for ms band *****/
+
+    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
+    readerXS->SetFileName(msfile);
+
+    /***** setup the filter *****/
+
+    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
+        <ImageType, VectorImageType, VectorIntImageType> FusionFilterType;
+
+    FusionFilterType::Pointer fusion = FusionFilterType::New();
+
+    /***** tie the inputs to the filter *****/
+
     fusion->SetPanInput(readerPAN->GetOutput());
     fusion->SetXsInput(readerXS->GetOutput());
 
-    WriterUint16Type::Pointer writer = WriterUint16Type::New();
+    /***** setup output *****/
 
-    writer->SetFileName(argv[3]);
+    typedef otb::StreamingImageFileWriter<VectorIntImageType> WriterType;
+
+    WriterType::Pointer writer = WriterType::New();
+
+    writer->SetFileName(outfile);
     writer->SetInput(fusion->GetOutput());
     writer->Update();
 
 }
 
-void workInt16(int argc, char* argv[]) {
+void pansharp_int16(char *panfile, char *msfile, char *outfile) {
+    
+    typedef otb::Image<double, 2>                   ImageType;
+    typedef otb::VectorImage<double, 2>             VectorImageType;
+    typedef otb::ImageFileReader<ImageType>         ReaderType;
+    typedef otb::ImageFileReader<VectorImageType>   ReaderVectorType;
+    typedef otb::VectorImage<GInt16, 2> VectorIntImageType;
+
+    /***** setup reader for pan band *****/
 
     ReaderType::Pointer       readerPAN = ReaderType::New();
-    readerPAN->SetFileName(argv[2]);
-    
-    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
-    readerXS->SetFileName(argv[3]);
+    readerPAN->SetFileName(panfile);
 
-    FusionInt16FilterType::Pointer fusion = FusionInt16FilterType::New();
+    /***** setup reader for ms band *****/
+
+    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
+    readerXS->SetFileName(msfile);
+
+    /***** setup the filter *****/
+
+    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
+        <ImageType, VectorImageType, VectorIntImageType> FusionFilterType;
+
+    FusionFilterType::Pointer fusion = FusionFilterType::New();
+
+    /***** tie the inputs to the filter *****/
+
     fusion->SetPanInput(readerPAN->GetOutput());
     fusion->SetXsInput(readerXS->GetOutput());
 
-    WriterInt16Type::Pointer writer = WriterInt16Type::New();
+    /***** setup output *****/
 
-    writer->SetFileName(argv[3]);
+    typedef otb::StreamingImageFileWriter<VectorIntImageType> WriterType;
+
+    WriterType::Pointer writer = WriterType::New();
+
+    writer->SetFileName(outfile);
     writer->SetInput(fusion->GetOutput());
     writer->Update();
 
 }
 
-void workUint32(int argc, char* argv[]) {
+
+void pansharp_uint32(char *panfile, char *msfile, char *outfile) {
+    
+    typedef otb::Image<double, 2>                   ImageType;
+    typedef otb::VectorImage<double, 2>             VectorImageType;
+    typedef otb::ImageFileReader<ImageType>         ReaderType;
+    typedef otb::ImageFileReader<VectorImageType>   ReaderVectorType;
+    typedef otb::VectorImage<GUInt32, 2> VectorIntImageType;
+
+    /***** setup reader for pan band *****/
 
     ReaderType::Pointer       readerPAN = ReaderType::New();
-    readerPAN->SetFileName(argv[2]);
-    
-    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
-    readerXS->SetFileName(argv[3]);
+    readerPAN->SetFileName(panfile);
 
-    FusionUint32FilterType::Pointer fusion = FusionUint32FilterType::New();
+    /***** setup reader for ms band *****/
+
+    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
+    readerXS->SetFileName(msfile);
+
+    /***** setup the filter *****/
+
+    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
+        <ImageType, VectorImageType, VectorIntImageType> FusionFilterType;
+
+    FusionFilterType::Pointer fusion = FusionFilterType::New();
+
+    /***** tie the inputs to the filter *****/
+
     fusion->SetPanInput(readerPAN->GetOutput());
     fusion->SetXsInput(readerXS->GetOutput());
 
-    WriterUint32Type::Pointer writer = WriterUint32Type::New();
+    /***** setup output *****/
 
-    writer->SetFileName(argv[3]);
+    typedef otb::StreamingImageFileWriter<VectorIntImageType> WriterType;
+
+    WriterType::Pointer writer = WriterType::New();
+
+    writer->SetFileName(outfile);
     writer->SetInput(fusion->GetOutput());
     writer->Update();
 
 }
 
-void workInt32(int argc, char* argv[]) {
+void pansharp_int32(char *panfile, char *msfile, char *outfile) {
+    
+    typedef otb::Image<double, 2>                   ImageType;
+    typedef otb::VectorImage<double, 2>             VectorImageType;
+    typedef otb::ImageFileReader<ImageType>         ReaderType;
+    typedef otb::ImageFileReader<VectorImageType>   ReaderVectorType;
+    typedef otb::VectorImage<GInt32, 2> VectorIntImageType;
+
+    /***** setup reader for pan band *****/
 
     ReaderType::Pointer       readerPAN = ReaderType::New();
-    readerPAN->SetFileName(argv[2]);
-    
-    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
-    readerXS->SetFileName(argv[3]);
+    readerPAN->SetFileName(panfile);
 
-    FusionInt32FilterType::Pointer fusion = FusionInt32FilterType::New();
+    /***** setup reader for ms band *****/
+
+    ReaderVectorType::Pointer readerXS = ReaderVectorType::New();
+    readerXS->SetFileName(msfile);
+
+    /***** setup the filter *****/
+
+    typedef otb::SimpleRcsPanSharpeningFusionImageFilter
+        <ImageType, VectorImageType, VectorIntImageType> FusionFilterType;
+
+    FusionFilterType::Pointer fusion = FusionFilterType::New();
+
+    /***** tie the inputs to the filter *****/
+
     fusion->SetPanInput(readerPAN->GetOutput());
     fusion->SetXsInput(readerXS->GetOutput());
 
-    WriterInt32Type::Pointer writer = WriterInt32Type::New();
+    /***** setup output *****/
 
-    writer->SetFileName(argv[3]);
+    typedef otb::StreamingImageFileWriter<VectorIntImageType> WriterType;
+
+    WriterType::Pointer writer = WriterType::New();
+
+    writer->SetFileName(outfile);
     writer->SetInput(fusion->GetOutput());
     writer->Update();
 
 }
 
+void usage(char *arg0) {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << arg0;
+    std::cerr <<
+        " < byte || uint16 || int16 || uint32 || int32 > <inputPanchromatiqueImage> <inputMultiSpectralImage> <outputImage>"
+         << std::endl;
+    exit( EXIT_FAILURE);
+}
+    
 int main(int argc, char* argv[]) {
-
-    // Software Guide : EndCodeSnippet
-
+    
     if (argc < 5)
     {
-        std::cerr << "Missing Parameters " << std::endl;
-        std::cerr << "Usage: " << argv[0];
-        std::cerr <<
-            " < byte || uint16 || int16 || uint32 || int32 > <inputPanchromatiqueImage> <inputMultiSpectralImage> <outputImage>"
-            << std::endl;
-        return EXIT_FAILURE;
+        usage(argv[0]);
     }    
 
-    if (! strcasecmp(argv[1], "byte")) {
-        workByte(argc, argv);
+    int type;
+    for( type = 0; type < GDT_TypeCount; type++ ) {
+
+        if ( GDALGetDataTypeName( (GDALDataType) type ) &&
+             EQUAL(GDALGetDataTypeName((GDALDataType) type), argv[1])
+           ) {
+            break;
+        }
     }
-    else if (! strcasecmp(argv[1], "uint16")) {
-        workUint16(argc, argv);
+
+    switch (type) {
+        
+        /***** byte *****/
+
+        case GDT_Byte:
+            pansharp_byte(argv[2], argv[3], argv[4]);
+            break;
+
+        /***** uint16 *****/
+        
+        case GDT_UInt16:
+            pansharp_uint16(argv[2], argv[3], argv[4]);
+            break;
+
+        /***** int16 *****/
+
+        case GDT_Int16:
+            pansharp_int16(argv[2], argv[3], argv[4]);
+            break;
+
+        /***** uint32 *****/
+
+        case GDT_UInt32:
+            pansharp_uint32(argv[2], argv[3], argv[4]);
+            break;
+
+        /***** int32 *****/
+
+        case GDT_Int32:
+            pansharp_int32(argv[2], argv[3], argv[4]);
+            break;
+        
+        default:
+            usage(argv[0]);
+            break;
     }
-    else if (! strcasecmp(argv[1], "int16")) {
-        workInt16(argc, argv);
-    }
-    else if (! strcasecmp(argv[1], "uint32")) {
-        workUint32(argc, argv);
-    }
-    else if (! strcasecmp(argv[1], "int32")) {
-        workInt32(argc, argv);
-    }
-    else {
-        std::cerr << "Missing Parameters " << std::endl;
-        std::cerr << "Usage: " << argv[0];
-        std::cerr <<
-            " < byte || uint16 || int16 || uint32 || int32 > <inputPanchromatiqueImage> <inputMultiSpectralImage> <outputImage>"
-            << std::endl;
-        return EXIT_FAILURE;
-    }
+
+
+    
+
 
     return EXIT_SUCCESS;
 }
